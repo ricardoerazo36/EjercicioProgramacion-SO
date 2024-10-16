2b222794588575f0d7917b832b5f76b7d59060c7
Ricardo Erazo Muñoz <ricardo.erazo@correounivalle.edu.co>
2024-10-16 16:50:36 -0500

    Update ProcessEjercicio.py

--- ProcessEjercicio.py
+++ ProcessEjercicio.py
@@ -1,7 +1,8 @@
-import multiprocessing
 from time import time
+import multiprocessing
+import sys
 
-# Función para calcular Fibonacci
+# Función para calcular el número de Fibonacci
 def fibonacci(n):
     if n <= 0:
         return 0
@@ -13,24 +14,45 @@ def fibonacci(n):
             a, b = b, a + b
         return b
 
-# Función que ejecuta el cálculo de Fibonacci en un proceso
-def calcular_fibonacci_en_proceso(vector, idx):
-    vector[idx] = fibonacci(vector[idx])
+# Clase para trabajadores utilizando procesos
+class FiboWorker(multiprocessing.Process):
+    def __init__(self, vector, idx, pid):
+        multiprocessing.Process.__init__(self)
+        self.vector = vector
+        self.idx = idx
+        self._pid = pid
+
+    def run(self):
+        print(f"[{self._pid}] Calculando Fibonacci de {self.vector[self.idx]}")
+        self.vector[self.idx] = fibonacci(self.vector[self.idx])
+        print(f"[{self._pid}] Fibonacci calculado es {self.vector[self.idx]}")
 
 def main():
-    vector = multiprocessing.Array('i', [33] * 144)
-    processes = []
-    ts = time()
-    
-    for i in range(len(vector)):
-        p = multiprocessing.Process(target=calcular_fibonacci_en_proceso, args=(vector, i))
-        processes.append(p)
-        p.start()
+    # Definir el tamaño del vector y el valor inicial
+    vector_size = 144
+    initial_value = 33
+    if len(sys.argv) > 1:
+        vector_size = int(sys.argv[1])
+    num_cpus = multiprocessing.cpu_count()  # CPUs disponibles
+    print(f"Calculando Fibonacci para {vector_size} posiciones en {num_cpus} CPUs")
 
-    for p in processes:
-        p.join()
+    # Inicializamos el vector de longitud `vector_size` con el valor `initial_value`
+    vector = multiprocessing.Array('i', [initial_value] * vector_size)
+    procesos = []  # Vector de procesos
 
-    print(f"Vector procesado con procesos: {list(vector)}")
+    ts = time()  # Se toma el tiempo
+    for i in range(vector_size):
+        pid = i % num_cpus  # Asignar IDs de proceso basado en el número de CPUs
+        worker = FiboWorker(vector, i, pid)
+        print(f"Trabajador {i} comienza para la posición {i}")
+        worker.start()
+        procesos.append(worker)
+
+    for i in range(vector_size):  # Ciclo para esperar por los trabajadores
+        print(f"Esperando por trabajador {i}")
+        procesos[i].join()
+
+    print(f"Vector procesado: {list(vector)}")
     print(f"Tardó {time() - ts} segundos")
 
 if __name__ == "__main__":
